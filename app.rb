88c3d22971b881adee21f2df148fbea1f5e7b527
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-12-28 15:44:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 class Application < Sinatra::Base
-  # Write your code here!
-
+  # Old route from previous tests
+  get '/' do
+    erb :index
+  end
+ 
+  # New route to respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
+  
+  get '/user_name' do
+    erb :user_name
+  end
+  
 end
