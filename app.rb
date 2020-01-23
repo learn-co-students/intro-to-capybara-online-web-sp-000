a6fe61a3016ed21fbf691f05a7292a90af3c7960
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-01-23 17:43:30 +0000

    final code with comments

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
+#./app.rb is our main application file, defining the controller that will power this web application.
+# We create an Application class and inherit from Sinatra::Base to give it all of the web superpowers needed to transform
+# the standard Ruby class into a Sinatra controller.
+
+#Within our Application controller, we use the Sinatra DSL to create a GET route at the / URL.
+# We tell our application to respond to HTTP GET requests to / by rendering the designated ERB template or HTML.
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index #tells the application to render, or deliver to the user's browser,
+               # the file in views/index.erb. This is a Sinatra-provided functionality that renders ERB templates located in the views directory
+  end
 
+  # New route to respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
+#Using the Sinatra post method, we create a response for requests to POST '/greet'. That response should be the HTML contained in the views/greet.erb template,
 end
--- config.ru
+++ config.ru
@@ -1,1 +1,14 @@
 # Write your code here!
+#our config.ru requires the sinatra gem. It then require_relatives the required file app.rb that defines our main Application controller.
+# Finally, we run our Application controller to start our web application.
+# When we run shotgun, the gem will look for a config.ru file if no other file is specified.
+# This file acts as the entry point for starting up your application.
+
+require 'sinatra'
+ 
+require_relative './app'
+ 
+run Application
+
+#config.ru file to put everything together and start our Sinatra application. This is the file shotgun or rackup will read to start your
+# local application server, and it's also the file our test suite is using to define our application –– remember Rack::Builder.parse_file('config.ru').first?
