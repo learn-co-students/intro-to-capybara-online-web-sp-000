f2551334f5cc7bf5508016c9b68f1eb9e8405e38
dayofgray <graysonpberger@gmail.com>
2020-05-21 17:28:18 -0400

    Revert "Done."
    This reverts commit f1b88e76dc045576b0f8173ebe95abbacf9955fa.

--- app.rb
+++ app.rb
@@ -1,11 +1,4 @@
 class Application < Sinatra::Base
-  # Old route from previous tests
-  get '/' do
-    erb :index
-  end
- 
-  # New route to respond to the form submission
-  post '/greet' do
-    erb :greet
-  end
+  # Write your code here!
+
 end
