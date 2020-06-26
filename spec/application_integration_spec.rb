fd2ffad4f0ab66268e49d9b93731667e677cab8d
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-26 22:36:26 +0000

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -7,7 +7,7 @@ describe "GET '/' - Greeting Form" do
     expect(page.body).to include("Welcome!")
   end
 
-  # New test
+  # New test visit method navigates the tests browser to a specific URL
   it 'has a greeting form with a user_name field' do
     visit '/'
 
@@ -26,3 +26,13 @@ describe "POST '/greet' - User Greeting" do
     expect(page).to have_text("Hi Avi, nice to meet you!")
   end
 end
+
+
+#Our new test has a similar setup to the first test– we tell Capybara to visit the page at '/'. Once that is done, we set some expectations against the page object that represent the user looking at the page in their browser. We can simply assert that the page has an HTML selector for form, meaning that the page contains an HTML element that matches the form tag.
+
+#Where does this magic have_selector matcher come from? That's right, Capybara has added that to RSpec. Capybara page objects respond to methods that relate intimately to HTML and the DOM (Document Object Model) that defines web applications. You can literally ask the page object: "Hey, do you have HTML that matches the following selector?"
+
+#visit '/'
+# we use the Capybara method fill_in to fill in the input field user_name with 'Avi'.
+
+# we click_button "Submit" to submit the form. That HTML interaction, submitting a form, will trigger a new HTTP request in the Capybara session and page object.
