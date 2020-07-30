67f32ea16de7621b38f40f81d0b156ccfc617a72
yarvinh <yarvinhhernandez@gmail.com>
2020-07-30 13:54:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
+  post '/greet' do
+    erb :greet
+  end
 
 end
+
+#learn
+# visit
+# page
+
+# have_selector
+# have_field
+# have_text
+
+# fill_in
+# click_button
+
+# diferencia entre 
+# get
+# post
