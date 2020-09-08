447771203289e41c11b49e920a8b7bdce09d96d6
sparsell <hello@sparsell.com>
2020-09-08 13:16:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
-class Application < Sinatra::Base
-  # Write your code here!
+class Application < Sinatra::Base #gives all methods to turn into a Sinatra controller
+  
+  get '/' do
+    erb :index
+  end
 
+  post '/greet' do
+    erb :greet
+  end
+  
 end
